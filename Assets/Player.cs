f60f0aabbf51f553223b55ca4661f98e97646971
Viktor Kornilov <39262485+ViktorKornilov@users.noreply.github.com>
2023-04-06 17:57:28 +0300

    Character Controller

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,44 +1,48 @@
-using System;
 using UnityEngine;
-
 
 public class Player : MonoBehaviour
 {
+    public float moveSpeed = 5;
     public float jumpSpeed = 5;
     private Rigidbody2D rb;
+    public LayerMask groundLayer;
+    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
+        var hit = Physics2D.Raycast(transform.position,Vector2.down,1f,groundLayer);
+        onGround = hit.transform != null;
+        
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rb.velocity.y);
+
         if (onGround && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)))
         {
             Jump();
         }
+
+        
+        if (rb.velocity.x != 0)
+        {
+            spriteRenderer.flipX = rb.velocity.x < 0;
+        }
+
+
     }
 
     public void Jump()
     {
-        print("jump");
         var jumpHeight = SpeedFromHeight(jumpSpeed);
         rb.velocity = Vector2.up * jumpHeight;
     }
 
 
     public bool onGround;
-    
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        onGround = true;
-    }
-
-    void OnCollisionExit2D(Collision2D other)
-    {
-        onGround = false;
-    }
     
     
     float SpeedFromHeight(float height)
